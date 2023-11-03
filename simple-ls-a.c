befb72d1b8dbb2f2c6cb5fd00482153d2963a40c
thanh123kte <aye123kte@gmail.com>
2023-11-03 23:15:32 +0700

    add sort_file_by_size in case'S'

--- simple-ls-a.c
+++ simple-ls-a.c
@@ -6,14 +6,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <sys/stat.h>
 
 DIR *dp;
 struct dirent *dirp;
 
 static void decode_switches(int argc, char **argv);
 void print_hidden_files();
+void sort_files_by_size();
+int compare_file_size(const void *a, const void *b);
+char *current_directory = NULL;
 
-int main(int argc, char* argv[]) {
+
+int main(int argc, char *argv[]) {
     int i;
 
     if (optind >= argc) {
@@ -21,9 +26,9 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // char* dir_name = argv[optind];
-    if ((dp = opendir(argv[1])) == NULL) {
-        fprintf(stderr, "Unable to open '%s': %s\n", argv[1], strerror(errno));
+    // Open the directory and check for errors
+    if ((dp = opendir(argv[optind])) == NULL) {
+        fprintf(stderr, "Unable to open '%s': %s\n", argv[optind], strerror(errno));
         exit(EXIT_FAILURE);
     }
 
@@ -34,30 +39,97 @@ int main(int argc, char* argv[]) {
 
 static void decode_switches(int argc, char **argv) {
     int c;
-    // làm option nào thì tự thêm dô, ví dụ như dưới là có sẵn a, l
-    while ((c = getopt(argc, argv, "al")) != -1) {
+    int sort_by_size = 0; // Flag to sort by file size
+
+    while ((c = getopt(argc, argv, "alS")) != -1) {
         switch (c) {
             case 'a':
-                //TODO
                 print_hidden_files();
                 break;
             case 'l':
-                //TODO
+                // TODO: Implement 'l' option
+                break;
+            case 'S':
+                sort_by_size = 1;
                 break;
             default:
-                fprintf(stderr, "Usage: %s [-a] [-l] dir_name\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-a] [-l] [-S] dir_name\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
+
+    // Check if there are additional arguments (i.e., the directory name)
+    if (optind < argc) {
+        current_directory = argv[optind];
+    }
+
+    if (sort_by_size) {
+        sort_files_by_size(current_directory);
+    }
 }
 
-void print_hidden_files(){
-    int show_hidden = 1;
-
+void print_hidden_files() {
     while ((dirp = readdir(dp)) != NULL) {
-        if (!show_hidden && dirp->d_name[0] == '.') {
-            continue; 
-        }
         printf("%s\n", dirp->d_name);
     }
+}
+
+void sort_files_by_size(const char *directory) {
+    // Open the specified directory
+    DIR *dp = opendir(directory);
+
+    if (dp == NULL) {
+        fprintf(stderr, "Unable to open '%s': %s\n", directory, strerror(errno));
+        return;
+    }
+
+    struct dirent **namelist;
+    int num_files = 0;
+
+    while ((dirp = readdir(dp)) != NULL) {
+        if (dirp->d_name[0] != '.') {  // Skip hidden files
+            num_files++;
+        }
+    }
+
+    if (num_files == 0) {
+        closedir(dp);
+        return;
+    }
+
+    // Rewind the directory stream to the beginning
+    rewinddir(dp);
+
+    namelist = (struct dirent **)malloc(num_files * sizeof(struct dirent *));
+
+    int i = 0;
+    while ((dirp = readdir(dp)) != NULL) {
+        if (dirp->d_name[0] != '.') {  // Skip hidden files
+            namelist[i++] = dirp;
+        }
+    }
+
+    qsort(namelist, num_files, sizeof(struct dirent *), compare_file_size);
+
+    for (int j = 0; j < num_files; j++) {
+        printf("%s\n", namelist[j]->d_name);
+    }
+
+    closedir(dp);
+
+    // Free memory used for namelist
+    free(namelist);
+}
+
+
+
+int compare_file_size(const void *a, const void *b) {
+    struct dirent entry_a = *(struct dirent *)a;
+    struct dirent entry_b = *(struct dirent *)b;
+
+    struct stat stat_a, stat_b;
+    stat(entry_a->d_name, &stat_a);
+    stat(entry_b->d_name, &stat_b);
+
+    return stat_b.st_size - stat_a.st_size; // Sort largest first
 }
